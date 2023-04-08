3f9cd939c3cc0ac3bb1424e3aa8a28df7802f28f
excel-ente <turkienich@gmail.com>
2023-04-07 22:07:19 -0300

    Configuraciones sqlite3

--- rpadmin/settings.py
+++ rpadmin/settings.py
@@ -77,6 +77,7 @@ WSGI_APPLICATION = 'rpadmin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -87,6 +88,14 @@ DATABASES = {
         'DATABASE_PORT':'5432',
     }
 }
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 10240 
 
